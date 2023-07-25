255932e6db4dc17b22ac435b794c2a9c91eeff0a
Christian Guether <christian.guether@softwareag.com>
2023-07-25 15:17:09 +0200

    add translations for en and de

--- i18n.ts
+++ i18n.ts
@@ -3,3 +3,5 @@
  * You can always add additional strings by adding your own po file. All po files are
  * combined to one JSON file per language and are loaded if the specific language is needed.
  */
+import '@c8y/ngx-components/locales/de.po';
+import '@c8y/ngx-components/locales/en.po';
--- src/indoor-air-quality-widget/iaq.service.ts
+++ src/indoor-air-quality-widget/iaq.service.ts
@@ -6,78 +6,77 @@ import { has, get } from 'lodash';
 
 @Injectable()
 export class IndoorAirQualityWidgetService {
-
   private readonly AIR_QUALITY_LEVELS: IndoorAirQualityLevel[] = [
     {
-      label: 'Excellent',
-      recommendation: 'No actions needed',
+      label: 'air-quality.excellent.label',
+      recommendation: 'air-quality.excellent.recommendation',
       threshold: {
         min: 0,
-        max: 50
+        max: 50,
       },
-      cssColor: '#00E400'
+      cssColor: '#00E400',
     },
     {
-      label: 'Good',
-      recommendation: 'No actions needed',
+      label: 'air-quality.good.label',
+      recommendation: 'air-quality.good.recommendation',
       threshold: {
         min: 51,
-        max: 100
+        max: 100,
       },
-      cssColor: '#92D050'
+      cssColor: '#92D050',
     },
     {
-      label: 'Lightly Polluted',
-      recommendation: 'Ventilation suggested',
+      label: 'air-quality.lightly-polluted.label',
+      recommendation: 'air-quality.lightly-polluted.recommendation',
       threshold: {
         min: 101,
-        max: 150
+        max: 150,
       },
-      cssColor: '#C9C900'
+      cssColor: '#C9C900',
     },
     {
-      label: 'Moderately Polluted',
-      recommendation: 'Increase ventilation with clean air',
+      label: 'air-quality.moderately-polluted.label',
+      recommendation: 'air-quality.moderately-polluted.recommendation',
       threshold: {
         min: 151,
-        max: 200
+        max: 200,
       },
-      cssColor: '#FF7E00'
+      cssColor: '#FF7E00',
     },
     {
-      label: 'Heavily Polluted',
-      recommendation: 'Optimize ventilation',
+      label: 'air-quality.heavily-polluted.label',
+      recommendation: 'air-quality.heavily-polluted.recommendation',
       threshold: {
         min: 201,
-        max: 250
+        max: 250,
       },
-      cssColor: '#FF0000'
+      cssColor: '#FF0000',
     },
     {
-      label: 'Severely Polluted',
-      recommendation: 'Maximize ventilation and reduce attendance',
+      label: 'air-quality.severely-polluted.label',
+      recommendation: 'air-quality.severely-polluted.recommendation',
       threshold: {
         min: 251,
-        max: 350
+        max: 350,
       },
-      cssColor: '#99004C'
+      cssColor: '#99004C',
     },
     {
-      label: 'Extremely Polluted',
-      recommendation: 'Avoid presence in room and maximize ventilation',
+      label: 'air-quality.extremely-polluted.label',
+      recommendation: 'air-quality.extremely-polluted.recommendation',
       threshold: {
         min: 351,
-        max: 500
+        max: 500,
       },
-      cssColor: '#663300'
-    }
+      cssColor: '#663300',
+    },
   ];
 
   public indoorAirQuality$: Subject<IndoorAirQualityLevel & { value: number }> = new Subject<
     IndoorAirQualityLevel & { value: number }
   >();
 
-  constructor(private measurementService: MeasurementService, private realtime: Realtime) { }
+  constructor(private measurementService: MeasurementService, private realtime: Realtime) {}
 
   init(deviceId: string, measurementFragment: string, measurementSeries: string) {
     this.loadLatestMeasurement(deviceId, measurementFragment, measurementSeries);
@@ -96,10 +95,10 @@ export class IndoorAirQualityWidgetService {
       valueFragmentType: measurementFragment,
       valueFragmentSeries: measurementSeries,
       pageSize: 1,
-      revert: true
+      revert: true,
     };
 
-    this.measurementService.list(filter).then(response => {
+    this.measurementService.list(filter).then((response) => {
       if (
         !response.data ||
         response.data.length != 1 ||
@@ -121,7 +120,7 @@ export class IndoorAirQualityWidgetService {
     measurementFragment: string,
     measurementSeries: string
   ) {
-    this.realtime.subscribe(`/measurements/${deviceId}`, measurementNotification => {
+    this.realtime.subscribe(`/measurements/${deviceId}`, (measurementNotification) => {
       const measurement: IMeasurement = measurementNotification.data.data;
       if (!measurement || !has(measurement, `${measurementFragment}.${measurementSeries}`)) {
         return;
@@ -137,7 +136,7 @@ export class IndoorAirQualityWidgetService {
 
   private updateIndoorAirQualityLevel(indoorAirQualityValue: number) {
     const indoorAirQualityLevel: IndoorAirQualityLevel = this.AIR_QUALITY_LEVELS.find(
-      airQualityLevel =>
+      (airQualityLevel) =>
         indoorAirQualityValue >= airQualityLevel.threshold.min &&
         indoorAirQualityValue <= airQualityLevel.threshold.max
     );
